76fd53cbb6a5d92be913f5c1a584115cc88a186d
gustavo-de-oliveira <gustavo_de_oliveir@usp.br>
2022-02-18 11:37:54 -0300

    versão teste de projeto com kafka

---
+++ Consumidor/Consumidor/Program.cs
@@ -0,0 +1,92 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+
+namespace Consumidor
+{
+    class Program
+    {
+        public class Book
+        {
+            public int IdBook { get; set; }
+            public string Author { get; set; }
+            public string Genre { get; set; }
+            public string Title { get; set; }
+
+            public Book(int id, string author, string genre, string title)
+            {
+                IdBook = id;
+                Author = author;
+                Genre = genre;
+                Title = title;
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            string getValor(string retorno, string key)
+            {
+                string valeu = "";
+                int i = retorno.LastIndexOf(key + ": ");
+                i += key.Length + 2;
+                while (i <= retorno.Length)
+                {
+                    char ch = retorno[i];
+                    if (ch == ',' || ch == ';')
+                        break;
+                    valeu += ch;
+                    i++;
+                }
+                return valeu;
+            }
+
+            var conf = new ConsumerConfig
+            {
+                GroupId = "test-consumer-group",
+                BootstrapServers = "127.0.0.1:9092",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using var c = new ConsumerBuilder<Ignore, string>(conf).Build();
+
+            {
+                c.Subscribe("book-topic");
+
+                var cts = new CancellationTokenSource();
+                Console.CancelKeyPress += (_, e) => {
+                    e.Cancel = true;
+                    cts.Cancel();
+                };
+
+                try
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            var cr = c.Consume(cts.Token);
+                            string str = cr.Value.ToString();
+
+                            Console.WriteLine($"Consumed message '{str}' at: '{cr.TopicPartitionOffset}'.");
+
+                            string chr = getValor(str, "IdBook").ToString();
+
+                            int i = Convert.ToInt32(chr.ToString().Trim());
+                            Book book = new Book(i, getValor(str, "Author"), getValor(str, "Genre"), getValor(str, "Title"));
+
+                            Console.WriteLine($"Obj = {{\n    IdBook = {book.IdBook};\n    Author = {book.Author};\n    Genre = {book.Genre};\n    Title = {book.Title};\n }}\n");
+                        }
+                        catch (ConsumeException e)
+                        {
+                            Console.WriteLine($"Error occured: {e.Error.Reason}");
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    c.Close();
+                }
+            }
+        }
+    }
+}
---
+++ Produtor/Produtor/Program.cs
@@ -0,0 +1,56 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Produtor
+{
+    class Program
+    {
+        public class Book {
+            public int IdBook { get; set; }
+
+            public string Author { get; set; }
+            public string Genre { get; set; }
+            public string Title { get; set; }
+        
+            public Book(int id, string author, string genre, string title)
+            {
+                IdBook = id;
+                Author = author;
+                Genre = genre;
+                Title = title;
+            }
+        }
+
+        public static async Task Main()
+        {
+            var config = new ProducerConfig { BootstrapServers = "127.0.0.1:9092" };
+
+            using var p = new ProducerBuilder<Null, string>(config).Build();
+
+            {
+                try
+                {
+                    var count = 0;
+                    while (true)
+                    {
+                        Book book = new Book(count, "Author " + count, "Genre " + count, "Title " + count);
+
+                        var dr = await p.ProduceAsync("book-topic",
+                            new Message<Null, string> { Value = $"IdBook: {book.IdBook}, Author: {book.Author}, Genre: {book.Genre}, Title: {book.Title};" });
+
+                        Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset} | {count}'");
+
+                        count++;
+                        Thread.Sleep(2000);
+                    }
+                }
+                catch (ProduceException<Null, string> e)
+                {
+                    Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                }
+            }
+        }
+    }
+}
